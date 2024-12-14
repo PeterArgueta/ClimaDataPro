83f1bf561b92c16cc119ee4234c8fd2d3cb3353b
PeterArgueta <peterarguedo@gmail.com>
2024-12-14 02:04:14 -0600

    modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -44,3 +44,4 @@ document.getElementById('menu-toggle').addEventListener('click', function () {
     const sidebar = document.getElementById('sidebar');
     sidebar.classList.toggle('active');
 });
+
