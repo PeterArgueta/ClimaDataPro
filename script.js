df493b3555462cf0d08963156946ff0003275896
PeterArgueta <peterarguedo@gmail.com>
2024-12-14 00:50:18 -0600

    modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function updateWeather(location = 'Guatemala') {
     const apiKey = 'fa0ed160c4624f128be184513241312';
     const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&lang=es`;
+
     fetch(url)
         .then(response => response.json())
         .then(data => {
@@ -9,33 +10,36 @@ function updateWeather(location = 'Guatemala') {
             document.getElementById('viento').textContent = `${data.current.wind_kph} km/h`;
             document.getElementById('fecha_hora').textContent = data.location.localtime;
             document.getElementById('descripcion').textContent = data.current.condition.text;
-            document.getElementById('location-name').textContent = data.location.name;
+            document.getElementById('location-name').textContent = data.location.name; // Actualiza el encabezado
         })
         .catch(error => console.error('Error al obtener datos:', error));
 }
 
-function updateForecast(location = 'Guatemala') {
-    const apiKey = 'fa0ed160c4624f128be184513241312';
-    const url = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=1&lang=es`;
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            const forecastDisplay = document.getElementById('forecast-display');
-            forecastDisplay.innerHTML = '';
-            const currentHour = new Date().getHours();
-            const hours = data.forecast.forecastday[0].hour.slice(currentHour, currentHour + 1);
-
-            hours.forEach(hour => {
-                const forecastCard = `
-                    <p>${hour.condition.text} <img src="https:${hour.condition.icon}" alt="${hour.condition.text}" style="width: 20px;"/></p>
-                `;
-                forecastDisplay.innerHTML += forecastCard;
-            });
-        })
-        .catch(error => console.error('Error al obtener el pronóstico:', error));
+function updateWeatherByGeolocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                const lat = position.coords.latitude;
+                const lon = position.coords.longitude;
+                const location = `${lat},${lon}`;
+                updateWeather(location);
+            },
+            error => {
+                console.error('Error en geolocalización:', error);
+                updateWeather(); // Usa "Guatemala" como predeterminado
+            }
+        );
+    } else {
+        console.error('Geolocalización no soportada');
+        updateWeather(); // Usa "Guatemala" como predeterminado
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    updateWeather();
-    updateForecast();
+    updateWeatherByGeolocation();
 });
+
+
+
+
+
